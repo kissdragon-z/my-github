e0f33610720e9b40ab2a13b14992db15eace3db0
netty-first-demo <kissdragon_z@126.com>
2019-03-07 13:56:07 +0800

    fa
    13123

--- java-basic/src/com/zml/thread/ThreadTest.java
+++ java-basic/src/com/zml/thread/ThreadTest.java
@@ -1,9 +1,70 @@
 package com.zml.thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ThreadTest {
 
     public static void main(String[] args) {
-        System.out.println("this is sys");
+
+
+        ExecutorService executorService_02 = Executors.newFixedThreadPool(5, new ThreadFactory() {
+
+            private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+
+            @Override
+            public Thread newThread(Runnable r) {
+
+                return new Thread(r, "test--------" + threadNumber.getAndIncrement());
+            }
+        });
+
+
+
+        for (int i = 0; i < 10; i++) {
+
+            executorService_02.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("--------");
+                    System.out.println("d:"+Thread.currentThread().getName());
+                    System.out.println("=============");
+
+                }
+            });
+        }
+
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        System.out.println("this is sys");
+//
+//
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//
+//        try {
+//
+//
+//            Thread.sleep(1);
+//
+//            t.wait();
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        t.start();
     }
 
 }
